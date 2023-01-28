63a03204ecd47288e3039a2d1976c511620e0b28
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-28 14:49:51 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/home_page.dart';
 
 void main() {
   runApp(flutter_demo());
 }
 
 class flutter_demo extends StatelessWidget {
-  String surname = "Makwana";
-  int  age= 19;
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello My Name is Piyush $surname & age is $age !"),
-          ),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
