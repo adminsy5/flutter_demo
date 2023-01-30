7ac0a9e132437b5e0fa07e61d5b34ea8575cc1f3
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-30 22:19:32 +0530

    day10

--- lib/models/items.dart
+++ lib/models/items.dart
@@ -14,17 +14,26 @@ class ItemsOfApp {
       required this.color,
       required this.image});
 
-  static final items = [
-    ItemsOfApp(
-      id: 1,
-      name: "iphone 12 Pro",
-      desc: "Apple iphone 12 Generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
-    )
-  ];
+  factory ItemsOfApp.fromMap(Map<String, dynamic> map) {
+    return ItemsOfApp(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
+
+  static List<ItemsOfApp>? items;
 }
 
 // class ItemsOfModel {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_demo/models/items.dart';
 import 'package:flutter_demo/widgets/drawer.dart';
 import 'package:flutter_demo/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final String surname = "Makwana";
   final int age = 19;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    // print(productsData);
+    ItemsOfApp.items = List.from(productsData)
+        .map<ItemsOfApp>((item) => ItemsOfApp.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +42,16 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: ItemsOfApp.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(itemsOfApp: ItemsOfApp.items[index]);
-          },
-        ),
+        child: (ItemsOfApp.items!.isNotEmpty && ItemsOfApp.items != null)
+            ? ListView.builder(
+                itemCount: ItemsOfApp.items?.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(itemsOfApp: ItemsOfApp.items![index]);
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
